25a31b89d710b1a8ff25fd617a157e541c617338
rapsby <gsu5022@gmail.com>
2019-01-28 11:47:47 -0800

    editing insert_student input form

--- Insert_student.php
+++ Insert_student.php
@@ -45,7 +45,7 @@
 	<tr>
 	<th width="120">id</th>
 	<td width="238">
-	<input type="text" name="txtId" value=<?php echo $id?>>
+	<input type="text" name="txtId" value=<?php echo $id?> readonly>
 	</td></tr>
 	<tr>
 	<th width="120">name</th>
@@ -67,62 +67,30 @@
 	<td><input type="text" name="txtPhoneNumber"></td></tr>
 	<th width="120">email</th>
 	<td><input type="text" name="txtEmail"></td></tr>
-	<tr>
-	<th width="120">startdate</th>
-	<td><input type="text" name="txtStartdate" ></td></tr>
-	<tr>
-	<th width="120">Lanfall Service Agreement</th>
-	<td><input type="text" name="txtLSA"></td></tr>
-	<tr>
-	<th width="120">Service tag</th>
-	<td><input type="text" name="txtTag"></td></tr>
+
+	<input type="hidden" name="txtStartdate" >
+
+	<input type="hidden" name="txtLSA" value="Y">
+
+	<input type="hidden" name="txtTag">
 	<tr>
 	<th width="120">Notes</th>
 	<td><input type="text" name="txtNotes"></td></tr>
-	<!-- <tr>
-	<th width="120">DocuSign</th>
-	<td> --><input type="hidden" name="txtDocuSign" value="Y"><!-- </td></tr> -->
-	<tr>
-	<th width="120">CPU</th>
-	<td><input type="text" name="txtCpu"></td></tr>
-	<tr>
-	<th width="120">Added to Ledger</th>
-	<td><input type="text" name="txtAddtoLed"></td></tr>
-	<!-- <tr>
-	<th width="120">Ordered</th>
-	<td> --><input type="hidden" name="txtOrdered" value="Y"><!-- </td></tr> -->
-	<!-- <tr>
-	<th width="120">Onhand</th>
-	<td> --><input type="hidden" name="txtOnhand" value="Y"><!-- </td></tr> -->
-	<!-- <tr>
-	<th width="120">LenApp</th>
-	<td> --><input type="hidden" name="txtLenApp" value="Y"><!-- </td></tr>
- -->	<!-- <tr>
-	<th width="120">TimApp</th>
-	<td> --><input type="hidden" name="txtTimApp" value="Y"><!-- </td></tr> -->
-	<tr>
-	<th width="120">PickUpDate</th>
-	<td><input type="text" name="txtPickUpDate"></td></tr>
-	<tr>
-	<th width="120">ShipDate</th>
-	<td><input type="text" name="txtShipDate"></td></tr>
-	<tr>
-	<th width="120">TrackingNumber</th>
-	<td><input type="text" name="txtTrackingNumber"></td></tr>
-	<!-- <tr> -->
-	<!-- <th width="120">Received</th> -->
-	<!-- <td> -->
-	<input type="hidden" name="txtReceived" value="N"><!-- </td> --><!-- </tr> -->
-	<!-- <tr> -->
-	<!-- <th width="120">Completed</th> -->
-	<!-- <td> -->
-	<input type="hidden" name="txtCompleted" value="Y"><!-- </td> --><!-- </tr> -->
-	<!-- <tr>
-	<th width="120">MSOFFICE</th>
-	<td> --><input type="hidden" name="txtMSOFFICE" value="Y"><!-- </td></tr> -->
-	<tr>
-	<th width="120">ReturnReceived</th>
-	<td><input type="text" name="txtReturnReceived"></td></tr>
+	<input type="hidden" name="txtDocuSign" value="Y">
+	<input type="hidden" name="txtCpu">
+	<input type="hidden" name="txtAddtoLed">
+	<input type="hidden" name="txtOrdered" value="Y">
+	<input type="hidden" name="txtOnhand" value="Y">
+	<input type="hidden" name="txtLenApp" value="Y">
+	<input type="hidden" name="txtTimApp" value="Y">
+	<input type="hidden" name="txtPickUpDate">
+	<input type="hidden" name="txtShipDate">
+	<input type="hidden" name="txtTrackingNumber">
+	<input type="hidden" name="txtReceived" value="N">
+	<input type="hidden" name="txtCompleted" value="Y">
+	<input type="hidden" name="txtMSOFFICE" value="Y">
+	
+	<input type="hidden" name="txtReturnReceived">
 	</table>
 
 	<h1></h1>
